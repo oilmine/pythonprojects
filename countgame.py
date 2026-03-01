0a4ec9efcff7028cd86cf010d756f9772453277f
oilmine <investmentslips2@gmail.com>
2026-03-01 14:59:14 +0530

    Countgame (BUGGED)

---
+++ countgame.py
@@ -0,0 +1,13 @@
+# NOTE: Very basic and bugged, don't use as reference.
+
+import time
+import random
+
+x = True
+
+while x == True:
+    u = int(input("u -> "))
+    print("")
+    time.sleep(0.5)
+    print("c ->", u + 1)
+
